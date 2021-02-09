38d1a962c3137c11aa85405184c8fb3d43936344
linusidom <linusidom@gmail.com>
2021-02-10 03:30:52 +0700

    django part8 detailview

--- django/firstproject/firstapp/urls.py
+++ django/firstproject/firstapp/urls.py
@@ -7,7 +7,12 @@ app_name = 'firstapp'
 
 urlpatterns = [
 	path('', views.firstapp_index, name='firstapp_index'),
-	path('firstapp_list_view', views.firstapp_list_view, name='firstapp_list_view'),
-	path('BookListView', views.BookListView.as_view(), name='book_list_view'),
+	path('book_list_view', views.firstapp_list_view, name='book_list_view'),
+	path('BookListView', views.BookListView.as_view(), name='BookListView'),
 
+
+	# Book Detail
+	path('book_detail_view/<int:pk>', views.book_detail_view, name='book_detail_view'),
+	# Class Based detail View
+	path('BookDetailView/<int:pk>', views.BookDetailView.as_view(), name='BookDetailView'),
 ]
--- django/firstproject/firstapp/views.py
+++ django/firstproject/firstapp/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from firstapp.models import Book
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def firstapp_index(request):
 	return render(request, 'firstapp/firstapp_index.html')	
 
+
+'''
+List View
+
+Function Based vs Class Based
+
+'''
 # Function Based View
 def firstapp_list_view(request):
 	books = Book.objects.all()
@@ -27,6 +35,47 @@ class BookListView(ListView):
 	# We have to have a file called book_list.html inside our firstapp/templates folder
 	# Otherwise we have to specify the name of the template we want to use
 		# template_name = 'firstapp/firstapp_list_view.html'
+
+
+
+
+'''
+Detail View
+
+Function Based vs Class Based
+
+'''
+
+# Function Based Detail View
+
+def book_detail_view(request, pk):
+	# I need a way to get the primary key of the book that I want to see the details of
+	book = Book.objects.get(pk=pk)
+
+	context = {
+		'book':book
+	}
+
+	return render(request, 'firstapp/book_detail_view.html', context)
+
+
+# Class Based View
+class BookDetailView(DetailView):
+	model = Book
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
