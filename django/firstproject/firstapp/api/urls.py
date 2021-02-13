22a98205d9488cf364036742d3d5cd93f2035c80
linusidom <linusidom@gmail.com>
2021-02-13 22:04:03 +0700

    django part 17 crud with apis

--- django/firstproject/firstapp/api/urls.py
+++ django/firstproject/firstapp/api/urls.py
@@ -9,4 +9,22 @@ urlpatterns = [
 	path('book_list_view', views.book_list_view, name='book_list_view'),
 	path('BookListViews', views.BookListView.as_view(), name='BookListView'),
 
+
+	# Detail
+	path('book_detail_view/<int:pk>', views.book_detail_view, name='book_detail_view'),
+	path('BookDetailView/<int:pk>', views.BookDetailView.as_view(), name='BookDetailView'),
+
+	# Create
+	path('book_create_view', views.book_create_view, name='book_create_view'),
+	path('BookCreateView', views.BookCreateView.as_view(), name='BookCreateView'),
+
+	# Update
+	path('book_update_view/<int:pk>', views.book_update_view, name='book_update_view'),
+	path('BookUpdateView/<int:pk>', views.BookUpdateView.as_view(), name='BookUpdateView'),
+
+
+	# Delete
+	path('book_delete_view/<int:pk>', views.book_delete_view, name='book_delete_view'),
+	path('BookDeleteView/<int:pk>', views.BookDeleteView.as_view(), name='BookDeleteView'),
+
 ]
--- django/firstproject/firstapp/api/views.py
+++ django/firstproject/firstapp/api/views.py
@@ -1,8 +1,21 @@
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from firstapp.api.models import BookSerializer
 from firstapp.models import Book
 
+
+from rest_framework.parsers import JSONParser
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+'''
+API List View
+'''
+
+# Function Based View
 def book_list_view(request):
 	books = Book.objects.all()
 	# books = Book.objects.get(pk=1)
@@ -24,6 +37,109 @@ def book_list_view(request):
 
 	return JsonResponse(book_serializer.data, safe=False)
 
+# Class Based View
 class BookListView(ListAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
+
+
+'''
+API Detail View
+'''
+# Function Based View
+def book_detail_view(request, pk):
+	# book = Book.objects.get(pk=pk)
+	book = get_object_or_404(Book, pk=pk)
+	# print(book.title, book.author, book.published_date, book.number_of_copies)
+	book_serializer = BookSerializer(book)
+	# print(book_serializer.data)
+	return JsonResponse(book_serializer.data, safe=False)
+
+# Class Based View
+class BookDetailView(RetrieveAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+
+
+
+
+'''
+API Create View
+'''
+
+# Function Create View
+@csrf_exempt
+def book_create_view(request):
+	data = JSONParser().parse(request)
+	serializer = BookSerializer(data=data)
+	if serializer.is_valid():
+		serializer.save()
+		return JsonResponse(serializer.data)
+	return JsonResponse(serializer.errors, status=400)
+
+
+# Class Based View
+
+class BookCreateView(CreateAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+
+
+
+'''
+API Update View
+'''
+# Function Update View
+@csrf_exempt
+def book_update_view(request, pk):
+	book = get_object_or_404(Book, pk=pk)
+	data = JSONParser().parse(request)
+	serializer = BookSerializer(book, data=data)
+	if serializer.is_valid():
+		serializer.save()
+		return JsonResponse(serializer.data)
+	return JsonResponse(serializer.errors, status=400)
+
+# Class Based View
+
+class BookUpdateView(UpdateAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+
+
+'''
+API Delete View
+'''
+
+@csrf_exempt
+def book_delete_view(request,pk):
+	book = get_object_or_404(Book, pk=pk)
+	book.delete()
+	return JsonResponse("{'message':'Deleted Successfully'}", safe=False)
+
+
+# Class Based
+class BookDeleteView(DestroyAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
