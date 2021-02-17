f80b6fc566aea43a2b0aa2ed815cacc299a9c34d
linusidom <linusidom@gmail.com>
2021-02-18 01:41:43 +0700

    django part23 updateview

--- django/firstproject/static/js/main.js
+++ django/firstproject/static/js/main.js
@@ -1,6 +1,6 @@
 div = document.querySelector('.insertJavaScriptHere')
 // console.log(div)
-
+bookListViewButton = document.querySelector('.bookListViewButton')
 
 // If we want to type text into JS
 // We need to use Backticks, left of number 1 key
@@ -50,7 +50,7 @@ function cardMaker(data){
 	// create a card header
 	let cardHeader = document.createElement('div')
 	cardHeader.setAttribute('class', 'card-header')
-	cardHeader.innerHTML = `${data.title}
+	cardHeader.innerHTML = `<a href="#" onClick="bookDetailView(${data.id})">${data.title}
 							Written By: ${data.author}`
 
 
@@ -58,7 +58,18 @@ function cardMaker(data){
 	let cardBody = document.createElement('div')
 	cardBody.setAttribute('class', 'card-body')
 	cardBody.innerHTML = `Published: ${data.published_date}
-						  Number of Copies: ${data.number_of_copies}`
+						  Number of Copies: ${data.number_of_copies}<br>`
+
+
+	// Buttons - Update
+	let updateButton = document.createElement('button')
+	updateButton.innerText = 'Update'
+	updateButton.setAttribute('class', 'btn btn-info mt-4 updateBookButton')
+	updateButton.setAttribute('id', `updateBookID-${data.id}`)
+	updateButton.setAttribute('data-bs-toggle', 'modal')
+	updateButton.setAttribute('data-bs-target', '#updateBookModal')
+
+	cardBody.append(updateButton)
 
 	// append card header and card-body to the Card
 	cardDiv.append(cardHeader)
@@ -67,6 +78,13 @@ function cardMaker(data){
 
 	return cardDiv
 }
+
+
+function pageReset(){
+	div.innerHTML = ``
+	bookListViewButton.style.display = 'none'
+}
+
 
 // Call information from the API
 async function listBook(){
@@ -80,6 +98,7 @@ function bookListView(){
 	listBook()
 	.then(data => {
 		console.log(data)
+		pageReset()
 		data.forEach(book => {
 			let card = cardMaker(book)
 			div.append(card)
@@ -88,7 +107,10 @@ function bookListView(){
 		})
 }
 
-bookListView()
+document.addEventListener('DOMContentLoaded', (e)=>{
+	bookListView()
+})
+	
 
 
 
@@ -130,23 +152,111 @@ document.querySelector('.book_create_save').addEventListener('click', (e) => {
 	createBook(book_data)
 	.then(data => {
 
-		console.log(data)
+		// console.log(data)
 		
-		div.innerHTML = ``
-
+		pageReset()
 		bookListView()
 
 	})
-
-
 
 })
 
 
 
+// Detail View
+async function detailBook(int){
+	let response = await fetch(`http://127.0.0.1:8000/firstapp_api/book_detail_view/${int}`)
+	let data = await response.json()
+	// console.log(data)
+	return data
+
+}
+
+function bookDetailView(int){
+	// console.log(int)
+
+	detailBook(int)
+	.then(data => {
+		// console.log(data)
+		
+		pageReset()
+		
+		bookListViewButton.style.display = '';
+
+		let card = cardMaker(data)
+		div.append(card)
+		
+	})
+}
 
 
 
+
+// Update View
+
+async function updateBook(postData, int){
+	let response = await fetch(`http://127.0.0.1:8000/firstapp_api/book_update_view/${int}`, {
+		method: 'POST',
+		headers: {
+			"Content-type":"application/json",
+		},
+		body: JSON.stringify(postData),
+	})
+}
+
+// Isolate the Button being clicked
+document.addEventListener('click', (e) => {
+	console.log(e.target.classList.contains('updateBookButton'))
+	if (e.target.classList.contains('updateBookButton')){
+		bookID = e.target.id.split('-').pop()
+		
+		console.log(bookID)
+
+		// First we have to use the DetailView to pull the existing data
+
+		detailBook(bookID)
+		.then(data => {
+
+			// Populate the fields in the modal
+
+			document.querySelector('#update_book_title').value = data.title
+			document.querySelector('#update_book_author').value = data.author
+			document.querySelector('#update_book_published_date').value = data.published_date
+			document.querySelector('#update_book_number_of_copies').value = data.number_of_copies
+
+			document.querySelector('.book_update_save').addEventListener('click', (e) => {
+				console.log(e)
+
+				// Get the updated Fields from the Modal
+
+				let book_title = document.querySelector('#update_book_title').value
+				let book_author = document.querySelector('#update_book_author').value
+				let book_published_date = document.querySelector('#update_book_published_date').value
+				let book_number_of_copies = document.querySelector('#update_book_number_of_copies').value
+				console.log(book_title, book_author, book_published_date, book_number_of_copies)
+
+				book_data = {
+					title: book_title,
+					author: book_author,
+					published_date: book_published_date,
+					number_of_copies: book_number_of_copies
+				}
+
+				// Update the backend via the API
+
+				updateBook(book_data, bookID)
+				.then(data => {
+
+					pageReset()
+					bookListView()
+
+				})
+			})
+
+		})
+
+	}
+})
 
 
 
