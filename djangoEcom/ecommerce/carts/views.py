fe34282f15b38d3635aa523459d61fd0880e4f49
linusidom <linusidom@gmail.com>
2021-04-07 18:09:19 +0700

    django ecom part 27 cc form in checkout

--- djangoEcom/ecommerce/carts/views.py
+++ djangoEcom/ecommerce/carts/views.py
@@ -4,6 +4,7 @@ from carts.models import Cart
 from courses.models import Course
 from orders.models import Order
 from billings.models import BillingProfile
+from cards.models import Card
 # Create your views here.
 
 def cart(request):
@@ -45,12 +46,19 @@ def cart_checkout(request):
 	# Order???
 	order, created = Order.objects.get_or_new(request=request, cart=cart, billing_profile=billing_profile)
 
-
+	# Card
+	card, created = Card.objects.get_or_new(request, billing_profile=billing_profile)
+	print('Card',card)
+	card_accepted = False
+	if request.method == 'POST':
+		card_accepted = request.POST.get('card_accepted', False)
 
 	context = {
 		'cart': cart,
 		'order': order,
-		'billing_profile': billing_profile
+		'billing_profile': billing_profile,
+		'card': card,
+		'card_accepted':card_accepted
 	}
 
 	return render(request, 'carts/cart_checkout.html', context)
