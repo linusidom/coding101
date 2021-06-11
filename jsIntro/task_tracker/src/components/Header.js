d6a4a83aca9d47f6c6133de8d8548ded159293e2
linusidom <linusidom@gmail.com>
2021-06-12 02:02:33 +0700

    react part 8 react bootstrap

--- jsIntro/task_tracker/src/components/Header.js
+++ jsIntro/task_tracker/src/components/Header.js
@@ -7,9 +7,15 @@ export default function Header(props) {
     // console.log(props)
     
     return(
-
-    // <div className="row">
-      // <div className="col">
+      // <div className="row">
+        // <div className="col">
+        // <h1>{props.title}</h1>
+        // </div>
+        // <div className="col text-end">
+        //   <button className={props.showForm ? 'btn btn-danger' :'btn btn-info'} onClick={props.toggleForm}>{props.showForm ? 'Close' : 'Add'}</button>
+        // </div>
+      // </div> 
+ 
     <Row>
       <Col>
         <h1>{props.title}</h1>
@@ -18,13 +24,7 @@ export default function Header(props) {
         <Button variant={props.showForm ? 'danger' :'info'} onClick={props.toggleForm}>{props.showForm ? 'Close' : 'Add'}</Button>
       </Col>
     </Row>
-        
-          
-        // </div>
-        // <div className="col text-end">
-        //   <button className={props.showForm ? 'btn btn-danger' :'btn btn-info'} onClick={props.toggleForm}>{props.showForm ? 'Close' : 'Add'}</button>
-        // </div>
-    // </div> 
+         
     )
 
 }
