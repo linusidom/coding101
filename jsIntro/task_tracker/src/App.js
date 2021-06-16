19ec0b66439e9493001615b97870159f96381680
linusidom <linusidom@gmail.com>
2021-06-16 14:13:31 +0700

    react part 9 alerts

--- jsIntro/task_tracker/src/App.js
+++ jsIntro/task_tracker/src/App.js
@@ -12,7 +12,7 @@ import { Tasks } from './components/Tasks';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container} from 'react-bootstrap'
+import {Container, Alert} from 'react-bootstrap'
 
 function App() {
 
@@ -38,142 +38,47 @@ function App() {
 
 
   function toggleForm(){
-    // console.log('Button To Toggle Clicked')
-    // console.log(showForm)
     setShowForm(!showForm)
-    // console.log(showForm)
   }
 
   function deleteTask(taskID){
-    // console.log('Delete Task', taskID)
-    
-    // This is Correct
     setTasks(tasks.filter( (task) => { return task.id !== taskID}  ))
-    
-    // This is Correct - No need for return statement if there is one conditional
-    // setTasks(tasks.filter( (task) => task.id !== taskID  ))
+    showAlertMessage('danger', 'Task Removed')
     
   }
-
 
   function addTask(task){
     setTasks(prevTasks => prevTasks.concat(task))
+    showAlertMessage('success', 'Task Added')
   }
 
-  // const [formData, setFormData] = useState('')
-  // function onSubmitForm(e){
-  //   e.preventDefault()
-  //   // console.log('Form Submitted')
-  //   // console.log(formData)
 
-  //   // Random Number for ID
-  //   // Math.random = 0 - 1, not inclusive, decimal float 
+  const [showAlert, setShowAlert] = useState(false)
+  const [alertVariant, setAlertVariant] = useState('warning')
+  const [alertMessage, setAlertMessage] = useState('This is a Message')
 
-  //   // console.log(Math.floor(Math.random() * 100 + 4))
-  //   const newTask = {
-  //     id: Math.floor(Math.random() * 100) + 4,
-  //     text: formData,
-  //   }
-  //   if(formData !== '') {
-  //     addTask(newTask)
-  //     setFormData('')
-  //   }
-    
-  // }
+  function showAlertMessage(variant, message) {
+    setShowAlert(true)
+    setAlertMessage(message)
+    setAlertVariant(variant)
 
-
-
+    setTimeout( () => {
+      setShowAlert(false)
+    }, 3000)
+  }
 
   return (
-    //  First Lets build the example template 
-    //  <div className="container my-5 p-5" style={{border:'2px solid green'}}> 
+ 
     <Container className="my-5 p-5" style={{border:'2px solid green'}}>
 
-        
-       {/* Header  */}
-
        <Header title='Task Tracker' showForm={showForm} toggleForm={toggleForm}/>
-       {/* <ConstHeader /> */}
-     	{/* <div className="row">
-        <div className="col">
-          <h1>Task Tracker</h1>
-        </div>
-        <div className="col text-end">
-          <button className={showForm ? 'btn btn-danger' :'btn btn-info'} onClick={toggleForm}>{showForm ? 'Close' : 'Add'}</button>
-        </div>
-      </div>  */}
 
-
-
-
-       {/* Form  */}
-
-      {/* Show 
-      { true && true }
-
-      {/* Do not show */}
-      {/* { false && true }  */}
+       {showAlert && <Alert variant={alertVariant}>{alertMessage}</Alert>}
 
        { showForm && <TaskForm addTask={addTask}/>}
 
-       {/* Item List  */}
-
-        {/* This is our desired end result */}
        <Tasks tasks={tasks} deleteTask={deleteTask}/>
 
-
-       {/* <div className="row">
-        <div className="col"> 
-
-          {
-             tasks.map((task) => { 
-              	return <div className="row  my-3" key={task.id}>
-                  <div className="col">
-                    <h3> { task.text} </h3>
-                  </div>
-                  <div className="col text-end">
-                    <button className='btn btn-danger' onClick={ () => deleteTask(task.id)}>Delete</button>
-                  </div>	
-                </div>
-            })
-          } */}
-
-
-           {/* Item 1  */}
-           {/* <div className="row  my-3">
-            <div className="col">
-              <h3>Item 1</h3>
-            </div>
-            <div className="col text-end">
-              <button className='btn btn-danger'>Delete</button>
-            </div>	
-          </div>  */}
-
-           {/* Item 2  */}
- 		    		{/* <div className="row  my-3">
-            <div className="col">
-              <h3>Item 2</h3>
-            </div>
-            <div className="col text-end">
-              <button className='btn btn-danger'>Delete</button>
-            </div>	
-          </div> */}
-
-          {/* Item 3  */}
-          {/* <div className="row my-3">
-            <div className="col">
-              <h3>Item 3</h3>
-            </div>
-            <div className="col text-end">
-              <button className='btn btn-danger'>Delete</button>
-            </div>	
-          </div> */}
-
-           {/* End Col Div  */}
-         {/* </div>  */}
-         {/* End Row Div  */}
-       {/* </div>  */}
-     {/* </div>  */}
      </Container>
   );
 }
